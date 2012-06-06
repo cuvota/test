4b8ec76f19b67ff9e15d4e8340e690b3e0e2c675
cuvota <cuvota@ba2.so-net.ne.jp>
2012-06-06 21:44:04 +0900

    set & print

--- test.pl
+++ test.pl
@@ -7,20 +7,24 @@ my %position;
 
 my $person;
 my $cards;
-my $bw;
-my number;
+my $color;
+my $number;
 for ($person=0; $person<4; $person++) {
-    for ($cards=0; $cards<4; $cards++) {
-        for ($number=0; $number<4; $number++) {
-            $position{"$person, $cards"} = "$cards, $number";
+    for ($cards=0; $cards<6; $cards++) {
+        for ($color=0; $color<2; $color++) {
+            for ($number=0; $number<4; $number++) {
+                $position{"$person, $cards"} = "$color, $number";
+	    }
         }
     }
 }
 
 for ($person=0; $person<4; $person++) {
     for ($cards=0; $cards<4; $cards++) {
-        for ($number=0; $number<4; $number++) {
-            print $position{"$person, $cards"} . "\n";
+        for ($bw=0; $bw<2; $bw++) {
+            for ($number=0; $number<4; $number++) {
+                print "\$position{\$person, \$cards\"\} = ". $position{"$person, $cards"} . "\n";
+	    }
         }
     }
 }
