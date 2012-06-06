155060533dbfd8bfb30cd1648782f26ccc7727e2
cuvota <cuvota@ba2.so-net.ne.jp>
2012-06-06 21:31:16 +0900

    多重for文を足した

--- test.pl
+++ test.pl
@@ -3,3 +3,25 @@ for ($n=0; $n < 4; $n++) {
     print "Hello, World!!\n";
 }
 
+my %position;
+
+my $person;
+my $cards;
+my $bw;
+my number;
+for ($person=0; $person<4; $person++) {
+    for ($cards=0; $cards<4; $cards++) {
+        for ($number=0; $number<4; $number++) {
+            $position{"$person, $cards"} = "$cards, $number";
+        }
+    }
+}
+
+for ($person=0; $person<4; $person++) {
+    for ($cards=0; $cards<4; $cards++) {
+        for ($number=0; $number<4; $number++) {
+            print $position{"$person, $cards"} . "\n";
+        }
+    }
+}
+
