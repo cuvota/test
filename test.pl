cc1de23f8cc73055067e8f1ecda34a6fb0ffe424
cuvota <cuvota@ba2.so-net.ne.jp>
2012-06-06 22:04:16 +0900

    code refine

--- test.pl
+++ test.pl
@@ -9,7 +9,7 @@ for ($person=0; $person<4; $person++) {
     for ($cards=0; $cards<6; $cards++) {
         for ($color=0; $color<2; $color++) {
             for ($number=0; $number<6; $number++) {
-                $position{"$person, $cards"} .= "{$color,$number}, ";
+                $position{"$person,$cards"} .= "{$color,$number},";
 	    }
         }
     }
@@ -17,7 +17,7 @@ for ($person=0; $person<4; $person++) {
 
 for ($person=0; $person<4; $person++) {
     for ($cards=0; $cards<6; $cards++) {
-        print "\$position{$person, $cards\"\} = ". $position{"$person, $cards"} . "\n";
+        print "{$person,$cards\}=". $position{"$person,$cards"} . "\n";
     }
 }
 
