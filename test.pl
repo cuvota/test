6e1c4ced8652745c07f10841e3364592c108c631
cuvota <cuvota@ba2.so-net.ne.jp>
2012-06-06 21:56:36 +0900

    debug

--- test.pl
+++ test.pl
@@ -1,10 +1,6 @@
-my $n;
-for ($n=0; $n < 4; $n++) {
-    print "Hello, World!!\n";
-}
+use strict;
 
 my %position;
-
 my $person;
 my $cards;
 my $color;
@@ -12,20 +8,16 @@ my $number;
 for ($person=0; $person<4; $person++) {
     for ($cards=0; $cards<6; $cards++) {
         for ($color=0; $color<2; $color++) {
-            for ($number=0; $number<4; $number++) {
-                $position{"$person, $cards"} = "$color, $number";
+            for ($number=0; $number<6; $number++) {
+                $position{"$person, $cards"} .= "{$color,$number}, ";
 	    }
         }
     }
 }
 
 for ($person=0; $person<4; $person++) {
-    for ($cards=0; $cards<4; $cards++) {
-        for ($bw=0; $bw<2; $bw++) {
-            for ($number=0; $number<4; $number++) {
-                print "\$position{\$person, \$cards\"\} = ". $position{"$person, $cards"} . "\n";
-	    }
-        }
+    for ($cards=0; $cards<6; $cards++) {
+        print "\$position{$person, $cards\"\} = ". $position{"$person, $cards"} . "\n";
     }
 }
 
